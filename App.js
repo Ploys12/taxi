a67cfaef0462bb555e7c41cf25f338e198dc78b8
Ploys12 <106439770+Ploys12@users.noreply.github.com>
2023-06-21 07:47:03 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import TaxiApp from './Components/TaxiApp';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <TaxiApp />
+    </View>
+  );
+};
+
+export default App;
